444c207df4699b9144f4367e5a05e2883259dc3d
fengdecong <785608942@qq.com>
2019-12-22 18:13:21 +0800

    判断是否是平衡二叉树

---
+++ leetcode110.java
@@ -0,0 +1,18 @@
+//判断是否是平衡二叉树  左右子树高度差不能超过1
+class Solution {
+    public int getHeight(TreeNode root){
+        if(root == null)
+        return 0;
+        return root == null ? 0 : Math.max(getHeight(root.left),getHeight(root.right))               + 1;
+        }
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)
+        return true;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if(Math.abs(left - right) < 2){
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+        return false;
+    }
+}
