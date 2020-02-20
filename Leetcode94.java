0934cf2d8b5d697c9cd707f93a619b5fa95b50f2
fengdecong <785608942@qq.com>
2020-02-20 11:08:02 +0800

    二叉树的中序遍历

---
+++ Leetcode94.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void _inOrder(TreeNode root,List<Integer> list){
+        if(root != null){
+            _inOrder(root.left,list);
+            list.add(root.val);
+            _inOrder(root.right,list);
+        }
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        _inOrder(root,list);
+        return list;
+    }
+}
